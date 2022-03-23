f346dae1f2502e322d0c67e90bdaf0cdc4104b8a
SomVariable <somevariable787898@gmail.com>
2022-03-23 16:09:50 +0200

    update timer

--- app.js
+++ app.js
@@ -1,43 +1,63 @@
-const newYearData = new Date('22 March 2022 16:21:13')
+const newYearData = new Date('23 March 2022 16:08:13')
+const countdown = (date, selector) => {
 
-const countdown = (date, selectors) => {
-
-    const selectorDays = document.querySelector(`.${selectors.days}`);
-    const selectorHours = document.querySelector(`.${selectors.hours}`);
-    const selectorMinutes = document.querySelector(`.${selectors.minutes}`);
-    const selectorSeconds = document.querySelector(`.${selectors.seconds}`);
-
-    const timeCont = () => {
+    const getTimeRemaining = (endTime) => {
         const currentDate = new Date()
-        const totalSeconds = Math.floor((date - currentDate) /1000);
+        const totalSeconds = Math.floor((endTime - currentDate) /1000);
 
-        const formatTime = (time) => time > 9? time: `0${time}`;
         const biggerThen0 = (action) => totalSeconds > 0 ?action : 0;
-
-        console.log(totalSeconds)
-        if(totalSeconds <= 0){
-            clearInterval(intervalId)
-        }
 
         const days = biggerThen0(Math.floor(totalSeconds /3600/24));
         const hours = biggerThen0(Math.floor(totalSeconds /3600) % 24);
         const minutes = biggerThen0(Math.floor(totalSeconds / 60) % 60);
         const seconds = biggerThen0(Math.floor(totalSeconds ) % 60);
         
-        selectorDays.textContent = formatTime(days);
-        selectorHours.textContent = formatTime(hours) ;
-        selectorMinutes.textContent = formatTime(minutes) ;
-        selectorSeconds.textContent = formatTime(seconds) ;
+        return {
+            totalSeconds,
+            days,
+            hours,
+            minutes,
+            seconds
+        } 
     }
 
-    const intervalId = setInterval(timeCont, 1000);
+    const setClock = (date, selector) => {
+        const cheackSelector = (parent, selector) => {
+            const classSelector = parent.querySelector(`.${selector}`)
+            const idSelector = parent.querySelector(`#${selector}`)
+            if(classSelector) return classSelector
+            else if(idSelector) return idSelector
+            else throw 'something wrong'  
+        }
+
+        const selecorCountdown = document.querySelector(selector),
+              selectorDays = cheackSelector(selecorCountdown, 'days'),
+              selectorHours = cheackSelector(selecorCountdown, 'hours'),
+              selectorMinutes = cheackSelector(selecorCountdown, 'minutes'),
+              selectorSeconds = cheackSelector(selecorCountdown, 'seconds');
+        
+        const intervalId = setInterval(updateClok, 1000);
+
+        function updateClok(){
+            const formatTime = (time) => time > 9? time: `0${time}`;
+
+            const time = getTimeRemaining(date)
+
+            selectorDays.textContent = formatTime(time.days);
+            selectorHours.textContent = formatTime(time.hours) ;
+            selectorMinutes.textContent = formatTime(time.minutes) ;
+            selectorSeconds.textContent = formatTime(time.seconds) ;
+
+            selectorDays.textContent = formatTime(time.days);
+            
+            if(time.totalSeconds <= 0) clearInterval(intervalId)
+        }
+    }
+
+    setClock(date, selector)
+
 }
 
-countdown(newYearData, {
-    days:'days',
-    hours:'hours',
-    minutes:'minutes',
-    seconds:'seconds'
-});
+countdown(newYearData, '.countdown-timer');
 
 
